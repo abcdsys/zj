499e85bd8c3d0af177e75b035096f789061c46c5
Moling <1970115881@qq.com>
2021-11-27 12:29:10 +0800

    Update MessAround.js

--- JavaScript/Tools/MessAround.js
+++ JavaScript/Tools/MessAround.js
@@ -1,6 +1,6 @@
 /* 
  *@authot:  Mol
- *create: 2021-11-24-20点20分
+ *@create: 2021-11-24-20/20-20
  */
 
 
@@ -9,7 +9,6 @@ var year = fishMan.getFullYear()
 var month = fishMan.getMonth();
 var d = fishMan.getDate();
 var h = fishMan.getHours()
-// console.log(`${month}///${d}`)
 
 function headInfo() {
     var mae = ''
@@ -21,22 +20,21 @@ function headInfo() {
         mae = '晚上'
     }
     console.log(`【摸鱼办】
-提醒您：${month + 1}月${d}日${mae}好，摸鱼人！工作再累，一定不要忘记摸鱼哦！有事没事起身去茶水间，去厕所，去廊道走走别老在工位上坐着，钱是老板的,但命是自己的`)
+提醒您：${month + 1}月${d}日${mae}好，摸鱼人！工作再累，一定不要忘记摸鱼哦！
+有事没事起身去茶水间，去厕所，去廊道走走别老在工位上坐着，钱是老板的,但命是自己的!`)
 }
 var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let getWeekDay = fishMan.getDay()
-let nowweekday = weekday[getWeekDay]
 
-function weekend(item) {
-    console.log(item)
+function weekend() {
+    let item = fishMan.getDay()
     if (item > 0 && item <= 5) {
         item = 6 - item
         console.log(`距离周末还有${item}天`)
     } else {
         console.log('好好享受周末吧')
     }
+    return item
 }
-
 
 function newYearSDay() {
     var nextNewYearDay = new Date(`${year + 1}`)
@@ -52,46 +50,31 @@ function newYearSDay() {
 }
 
 function chingMingFestival() {
-    let nextChingMingFestival = new Date(`${year + 1},3,4`)
+    let nextChingMingFestival = new Date(`${year + 1},4,4`)
     let startDate = Date.parse(fishMan);
     let endDate = Date.parse(nextChingMingFestival);
     let days = Math.round((endDate - startDate) / (1 * 24 * 60 * 60 * 1000));
     if ((month == 3) && (d == 4)) {
         console.log(`今天就是清明节`)
-
     } else {
         console.log(`距离清明节还有${days}天`)
-
     }
     return days
 }
+async function battle() {
 
-
-
+}
 
 function lastInfo() {
-    console.log(`上班是帮老板赚钱，摸鱼是赚老板的钱！最后，祝愿天下所有摸鱼人，都能愉快的渡过每一天…​`)
+    let info = `上班是帮老板赚钱，摸鱼是赚老板的钱！最后，祝愿天下所有摸鱼人，都能愉快的渡过每一天…​`
+    console.log(info)
+    return info
 }
 
-
-
-
-function getOneSpeech() {
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            myObj = JSON.parse(this.responseText);
-            console.log(myObj)
-        }
-    };
-    xmlhttp.open("GET", "https://v1.hitokoto.cn/", true);
-    xmlhttp.send();
-
-}
-// getOneSpeech()
 
 function main() {
-    headInfo();
-    weekend(getWeekDay);
+    headInfo()
+    weekend()
     newYearSDay()
     chingMingFestival()
     lastInfo()
