3798a660d17c8f58f2fdaf3488e9cc80120d7124
Moling <1970115881@qq.com>
2021-11-27 12:41:17 +0800

    Create weDay.js

---
+++ JavaScript/Tools/weDay.js
@@ -0,0 +1,9 @@
+/* 
+ *@author:Mol
+ *@create:12点38分
+ */
+var weDay = new Date();
+var year = fishMan.getFullYear()
+var month = fishMan.getMonth();
+var day = fishMan.getDate();
+var hour = fishMan.getHours()
