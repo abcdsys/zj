651811877f111115e9830c2d6101487085575789
Moling <1970115881@qq.com>
2021-12-06 20:13:05 +0800

    Update MessAround.js

--- JavaScript/Tools/MessAround.js
+++ JavaScript/Tools/MessAround.js
@@ -22,7 +22,7 @@ function headInfo() {
     }
     let info = `【摸鱼办】
 提醒您：${month + 1}月${day}日${mae}好，摸鱼人！工作再累，一定不要忘记摸鱼哦！
-有事没事起身去茶水间， 去厕所， 去廊道走走别老在工位上坐着， 钱是老板的, 但命是自己的!\n `
+有事没事起身去茶水间， 去厕所， 去廊道走走别老在工位上坐着， 钱是老板的, 但命是自己的! `
     msg += info
     return info
 }
@@ -32,16 +32,16 @@ function weekend() {
     let info = ``
     if (item > 0 && item <= 5) {
         item = 6 - item
-        info = `距离周末还有${item}天\n`
+        info = `\n距离周末还有${item}天\n`
     } else {
-        info = '好好享受周末吧\n'
+        info = `\n好好享受周末吧\n`
     }
     msg += info
     return info
 }
 var startDate = Date.parse(fishMan);
 
-function festival(chinese, fmonth, fday) {
+function festival([chinese, fmonth, fday]) {
     let info = ``
     let newfestival = new Date(`${year + 1},${fmonth},${fday}`)
     let endDate = Date.parse(newfestival);
@@ -61,24 +61,20 @@ function lastInfo() {
     return info
 }
 
-async function test() {
-    let festival = [
+async function festivalAll() {
+    let festivalList = [
         ['元旦', 1, 1],
         ['清明', 4, 4]
     ]
-    const n = festival.length;
-    for (let i = 0; i <= n; i++) {
-        console.log(festival[i])
+    const n = festivalList.length;
+    for (let i = 0; i < n; i++) {
+        festival(festivalList[i])
     }
 }
 (async function main() {
     headInfo()
     weekend()
-    await test()
-    /* festival('元旦', 1, 1)
-    festival('清明', 4, 4)
-    festival('劳动', 5, 1)
-    festival('国庆', 10, 1) */
+    festivalAll()
     lastInfo()
     console.log(msg)
 })()
