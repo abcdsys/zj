0e35819f55be41320b7da2a9932165e1744decb6
Moling <1970115881@qq.com>
2021-12-29 00:46:27 +0800

    修改检测节日是否度过

--- JavaScript/Tools/MessAround.js
+++ JavaScript/Tools/MessAround.js
@@ -6,7 +6,7 @@
 
 var fishMan = new Date(),
     year = fishMan.getFullYear(),
-    month = fishMan.getMonth(),
+    month = fishMan.getMonth() + 1,
     day = fishMan.getDate(),
     hour = fishMan.getHours();
 var msg = ''
@@ -21,8 +21,8 @@ function headInfo() {
         mae = '晚上'
     }
     let info = `【摸鱼办】
-提醒您：${month + 1}月${day}日${mae}好，摸鱼人！工作再累，一定不要忘记摸鱼哦！
-有事没事起身去茶水间， 去厕所， 去廊道走走别老在工位上坐着， 钱是老板的, 但命是自己的! `
+提醒您：${month }月${day}日${mae}好，摸鱼人！工作再累，一定不要忘记摸鱼哦！
+有事没事起身去茶水间， 去厕所， 去廊道走走别老在工位上坐着， 钱是老板的, 但命是自己的!`
     msg += info
 }
 
@@ -38,15 +38,41 @@ function weekend() {
     msg += info
 }
 var startDate = Date.parse(fishMan);
+/**
+ * @Created by Mol on 2021/12/29
+ * @description 等待优化
+ */
 
 function festival([chinese, fmonth, fday]) {
-    let info = ``
-    let newfestival = new Date(`${year + 1},${fmonth},${fday}`)
-    let endDate = Date.parse(newfestival);
-    let days = Math.round((endDate - startDate) / (1 * 24 * 60 * 60 * 1000));
+    let info = 
+    function calculate(endDate) {
+        return Math.round((endDate - startDate) / (1 * 24 * 60 * 60 * 1000));
+    }
     if (month == fmonth && day == fday) {
         info = `今天就是${chinese}节，好好享受！\n`
-    } else {
+    } else if (month < fmonth) {
+        let newFestivalData = new Date(`${year},${fmonth},${fday}`)
+        let endDate = Date.parse(newFestivalData);
+        let days = calculate(endDate)
+        info = `距离${chinese}节还有${days}天\n`
+    } else if (month == fmonth && day < fday) {
+        let newFestivalData = new Date(`${year},${fmonth},${fday}`)
+        let endDate = Date.parse(newFestivalData);
+        let days = calculate(endDate); info = `距离${chinese}节还有${days}天\n`
+    } else if (month == fmonth && day > fday) {
+        let newFestivalData = new Date(`${year + 1},${fmonth},${fday}`)
+        let endDate = Date.parse(newFestivalData);
+        let days = calculate(endDate);
+        info = `距离${chinese}节还有${days}天\n`
+    } else if (month > fmonth) {
+        let newFestivalData = new Date(`${year + 1},${fmonth},${fday}`)
+        let endDate = Date.parse(newFestivalData);
+        let days = calculate(endDate);
+        info = `距离${chinese}节还有${days}天\n`
+    } else if (month < fmonth && day < fday) {
+        let newFestivalData = new Date(`${year + 1},${fmonth},${fday}`)
+        let endDate = Date.parse(newFestivalData);
+        let days = calculate(endDate);
         info = `距离${chinese}节还有${days}天\n`
     }
     msg += info
@@ -55,20 +81,22 @@ function festival([chinese, fmonth, fday]) {
 function lastInfo() {
     let info = `上班是帮老板赚钱，摸鱼是赚老板的钱！最后，祝愿天下所有摸鱼人，都能愉快的渡过每一天…\n​`
     msg += info
-    
+
 }
 
 function festivalAll() {
     let festivalList = [
         ['元旦', 1, 1],
-        ['清明', 4, 4]
+        ['清明', 4, 4],
+        ['劳动', 5, 1],
+        ['国庆', 10, 11],
     ]
     const n = festivalList.length;
     for (let i = 0; i < n; i++) {
         festival(festivalList[i])
     }
 }
-(function main() {
+(function () {
     headInfo()
     weekend()
     festivalAll()
