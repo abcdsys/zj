dd68e3ec9a19a71d4f72204b9901407f4d928310
Moling <1970115881@qq.com>
2022-01-03 23:50:33 +0800

    Update MessAround.js

--- JavaScript/Tools/MessAround.js
+++ JavaScript/Tools/MessAround.js
@@ -3,6 +3,20 @@
  *@create: 2021-11-24-20/20-20
  */
 
+ /**
+  * @Created by Mol on 2022/01/03
+  * @description 节假日列表
+  */
+ 
+    let festivalList = [
+        ['元旦', 1, 1],
+        ['清明', 4, 5],
+        ['劳动', 5, 1],
+        ['国庆', 10, 1],
+        ['圣诞', 12, 25],
+        ['情人', 2, 14],
+        ['妇女', 3, 1],
+    ]
 
 var fishMan = new Date(),
     year = fishMan.getFullYear(),
@@ -78,15 +92,7 @@ function lastInfo() {
 }
 
 function festivalAll() {
-    let festivalList = [
-        ['元旦', 1, 1],
-        ['清明', 4, 5],
-        ['劳动', 5, 1],
-        ['国庆', 10, 1],
-        ['圣诞', 12, 25],
-        ['情人', 2, 14],
-        ['妇女', 3, 1],
-    ]
+
 
     function paixu([...arr]) {
 
